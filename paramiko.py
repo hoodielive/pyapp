1c1604d8dbf80190e27562a21d390764ddea342e
hoodielive <hoodielive@gmail.com>
2018-03-03 21:33:03 -0500

    paramkio::SSH

---
+++ paramiko.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import getpass
+import paramiko
+HOSTNAME = 'rhsrv01.rhce.lab'
+PORT = 22
+
+def run_ssh_cmd(username, password, cmd, hostname=HOSTNAME,
+        port=PORT):
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(parmiko.AutoAddPolicy())
+    ssh_client.load_system_host_keys()
+    ssh_client.connect(hostname, port, username, password)
+    stdin, stdout, stderr = ssh_client.exec_command(cmd)
+    print(stdout.read())
+
+
+if __name__ = '__main__':
+    username = input("Enter username: ")
+    password = getpass.getpass(prompt="Enter password: ")
+    cmd = 'ls -l /dev'
+    run_ssh_cmd(username, password, cmd)
